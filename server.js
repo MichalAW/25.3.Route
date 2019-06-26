3c37fab99eca5bb750e4cd96588236a230ae1635
MichalAW <michal.wantaeuco@gmail.com>
2019-06-26 15:32:24 +0200

    bug req

--- server.js
+++ server.js
@@ -5,19 +5,23 @@ var stringifyFile = req.params.note();
 
 app.use(bodyParser.json());
 
-fs.readFile('./test.json', 'utf8', function (err, data) {
-    if (err) throw err;
-    stringifyFile = data
-    res.send(data);
+app.get('/', function () {
+    fs.readFile('./test.json', 'utf8', function (err, data) {
+        if (err) throw err;
+        stringifyFile = data
+        res.send(data);
+    });
 });
 
-app.post('/updateNote/:note', function () {
-    res.send('Hello POST!');
-});
-
-fs.writeFile('./test.json', stringifyFile, function (err) {
-    if (err) throw err
-    console.log('file updated');
+app.post('/updateNote/:stringifyFile', function () {
+    fs.writeFile('./test.json', stringifyFile, function (err) {
+        if (err) throw err
+        console.log('file updated');
+    });
 });
 
 app.listen(3000);
+
+app.use(function (res, next) {
+    res.status(404).send('sorry, I dont know what you mean')
+});
