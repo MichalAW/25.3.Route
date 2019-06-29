4e1f5294038af2a2c6578193e9575dcee011b13c
MichalAW <michal.wantaeuco@gmail.com>
2019-06-29 08:32:33 +0200

    fix post

--- server.js
+++ server.js
@@ -2,22 +2,32 @@ var fs = require('fs')
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var stringifyFile;
 
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    var stringifyFile = req.params.note;
-});
-
-app.post('/updateNote/:stringifyFile', function (req, res) {
     fs.readFile('./test.json', 'utf8', function (err, data) {
         if (err) throw err;
         stringifyFile = data
         res.send(data);
     });
-    fs.writeFile('./test.json', stringifyFile, function (err) {
-        if (err) throw err
-        console.log('file updated');
+});
+
+app.post('/updateNote/:note', function (req, res) {
+    fs.readFile('./test.json', 'utf8', function (err, data) {
+        
+        if (err) throw err;
+
+        stringifyFile = data + req.params.note;
+
+        fs.writeFile('./test.json', stringifyFile, function (err) {
+
+            if (err) throw err;
+
+            console.log('file updated');
+            res.send(stringifyFile);
+        });
     });
 });
 
